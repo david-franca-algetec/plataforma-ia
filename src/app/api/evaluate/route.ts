a8f292bc01b6f159617d73664bb4c2d4261259cf
david-franca-algetec <emanuelcrisostomo@outlook.com>
2024-03-25 15:17:25 -0300

    fix(evaluate): check rounds

--- src/app/api/evaluate/route.ts
+++ src/app/api/evaluate/route.ts
@@ -3,14 +3,14 @@ import {genSalt, hash} from 'bcrypt'
 export async function GET() {
   const API_URL = process.env.API_URL
   const API_KEY = process.env.API_KEY
+  const SALT_ROUNDS = process.env.SALT_ROUNDS || '10'
 
-  if (!API_URL || !API_KEY) {
-    return new Response('API_URL or API_KEY is not defined', {status: 500})
+  if (!API_URL || !API_KEY || !SALT_ROUNDS) {
+    return new Response('API_URL, API_KEY or SALT_ROUNDS is not defined', {status: 500})
   }
 
   try {
-    const saltRounds = process.env.SALT_ROUNDS || '10'
-    const salt = await genSalt(Number(saltRounds))
+    const salt = await genSalt(Number(SALT_ROUNDS))
     const hashedKey = await hash(API_KEY, salt)
 
     return Response.json({key: hashedKey, url: API_URL}, {status: 200})
